19be43a03add91f1f4264a385ce0eb467f7026ed
brunomy <brunopuc2015@gmail.com>
2024-10-12 17:53:22 -0300

    [responsive] cadastro-endereco

--- src/app/components/cadastro-cartao/cadastro-cartao.component.ts
+++ src/app/components/cadastro-cartao/cadastro-cartao.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { InputComponent } from '../formComponents/input/input.component';
-import { SubmitButtonComponent } from '../formComponents/submit-button/submit-button.component';
 
 @Component({
   selector: 'app-cadastro-cartao',
   standalone: true,
-  imports: [InputComponent, SubmitButtonComponent],
+  imports: [InputComponent],
   templateUrl: './cadastro-cartao.component.html',
   styleUrl: './cadastro-cartao.component.scss'
 })
--- src/app/components/cadastro-endereco/cadastro-endereco.component.ts
+++ src/app/components/cadastro-endereco/cadastro-endereco.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { InputComponent } from '../formComponents/input/input.component';
-import { SubmitButtonComponent } from '../formComponents/submit-button/submit-button.component';
 
 @Component({
   selector: 'app-cadastro-endereco',
   standalone: true,
-  imports: [InputComponent, SubmitButtonComponent],
+  imports: [InputComponent],
   templateUrl: './cadastro-endereco.component.html',
   styleUrl: './cadastro-endereco.component.scss'
 })
