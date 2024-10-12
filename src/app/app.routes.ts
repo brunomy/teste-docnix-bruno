401eaaede6a5bf77f3ac58d463b7446ca8596cde
brunomy <brunopuc2015@gmail.com>
2024-10-12 18:49:24 -0300

    [page, responsive] cadastro-finalizado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { CadastroComponent } from './pages/cadastro/cadastro.component';
 import { CadastroCartaoComponent } from './components/cadastro-cartao/cadastro-cartao.component';
+import { CadastroFinalizadoComponent } from './pages/cadastro-finalizado/cadastro-finalizado.component';
 
 export const routes: Routes = [
     {
@@ -13,11 +14,15 @@ export const routes: Routes = [
         component: CadastroComponent
     },
     {
-        path: "cadastro-endereco",
+        path: "cadastro/endereco",
         component: CadastroComponent
     },
     {
-        path: "cadastro-cartao",
+        path: "cadastro/cartao",
         component: CadastroComponent
+    },
+    {
+        path: "cadastro-finalizado",
+        component: CadastroFinalizadoComponent
     },
 ];
---
+++ src/app/pages/cadastro-finalizado/cadastro-finalizado.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { ImgFileComponent } from '../../components/formComponents/img-file/img-file.component';
+
+@Component({
+  selector: 'app-cadastro-finalizado',
+  standalone: true,
+  imports: [ImgFileComponent],
+  templateUrl: './cadastro-finalizado.component.html',
+  styleUrl: './cadastro-finalizado.component.scss'
+})
+export class CadastroFinalizadoComponent {
+
+}
